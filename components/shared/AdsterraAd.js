9748e52f187e7991089cc566924f6f3eb540585c
Stephen Karuku <s.karuku.sk@gmail.com>
2026-05-02 12:37:29 +0300

    updates

--- components/shared/AdsterraAd.js
+++ components/shared/AdsterraAd.js
@@ -4,7 +4,7 @@ const AdsterraAd = () => {
   return (
     <div style={{ width: '100%', minHeight: '100%' }}>
       <Script
-        src="https://pl25562030.profitablecpmratenetwork.com/23/c6/99/23c6999750507ae026e9a271af24ea8d.js"
+        src="https://pl29316724.profitablecpmratenetwork.com/21/e9/5e/21e95e1b8ccf0c28affa1bfbd09bee09.js"
         strategy="afterInteractive"
       />
     </div>
